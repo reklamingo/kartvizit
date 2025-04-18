9fff7a3cb17835101ba55010658b1e29b2e2e518
reklamingo <arifkurtulmuslar@gmail.com>
2025-04-18 14:48:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,7 @@ def upload():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
